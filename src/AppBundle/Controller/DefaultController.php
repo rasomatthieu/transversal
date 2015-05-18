dc189858b988f541ae00318827c95fb48abe6a16
AlexandreSimonet <abx24z4hb9>
2015-05-18 11:32:04 +0200

    Create command system

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,10 +2,13 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Command;
 use AppBundle\Entity\Information;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class DefaultController extends Controller
 {
@@ -20,8 +23,12 @@ class DefaultController extends Controller
     /**
      * @Route("/offre", name="offre")
      */
-    public function offreAction()
+    public function offreAction(Request $request)
     {
+        if ($request->getMethod() == 'POST') {
+
+        }
+
         return $this->render('AppBundle:Default:offre.html.twig');
     }
 
@@ -30,6 +37,12 @@ class DefaultController extends Controller
      */
     public function coordonneeAction(Request $request)
     {
+        $Session = new Session();
+
+        /*if ($Session->get('etape') != 2) {
+            return new RedirectResponse($this->generateUrl('offre'));
+        }*/
+
         if ($request->getMethod() == 'POST') {
             $data = $request->request->get('request');
             $em = $this->getDoctrine()->getManager();
@@ -49,6 +62,13 @@ class DefaultController extends Controller
             $Information->setPhonefix($data['phonefix']);
             $Information->setPhonemobile($data['phonemobile']);
             $Information->setZip($data['zip']);
+
+            $em->persist($Information);
+            $em->flush();
+
+            $Session->set('etape', 3);
+
+            return new RedirectResponse($this->generateUrl('paiement'));
         }
         return $this->render('AppBundle:Default:coordonnee.html.twig');
     }
@@ -56,8 +76,34 @@ class DefaultController extends Controller
     /**
      * @Route("/paiement", name="paiement")
      */
-    public function paiementAction()
+    public function paiementAction(Request $request)
     {
+        $Session = new Session();
+
+        /*if ($Session->get('etape') != 3) {
+            return new RedirectResponse($this->generateUrl('offre'));
+        }*/
+
+        if ($request->getMethod() == 'POST') {
+            $data = $request->request->get('request');
+            $em = $this->getDoctrine()->getManager();
+
+            $Offer = $em->getRepository('AppBundle:Offer')->find($Session->get('offer'));
+            $Payment = $em->getRepository('AppBundle:Payment')->find($data['payment']);
+
+            $Command = new Command();
+            $Command->setZip($data['zip']);
+            $Command->setLastname($data['lastname']);
+            $Command->setFirstname($data['fistname']);
+            $Command->setAddress($data['adress']);
+            $Command->setCountry($data['country']);
+            $Command->setPhone($data['phone']);
+            $Command->setOffer($Offer);
+            $Command->setPayment($Payment);
+
+            $em->persist($Command);
+            $em->flush();
+        }
         return $this->render('AppBundle:Default:paiement.html.twig');
     }
 }
