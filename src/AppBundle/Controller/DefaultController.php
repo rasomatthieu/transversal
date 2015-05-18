58689d2585f8c95cbad3c76ea338c66676f3ab59
AlexandreSimonet <abx24z4hb9>
2015-05-18 11:09:09 +0200

    Create page offre, coordonnee, payment with function in controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,8 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Information;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -13,5 +15,49 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('AppBundle:Default:homepage.html.twig');
+    }
+
+    /**
+     * @Route("/offre", name="offre")
+     */
+    public function offreAction()
+    {
+        return $this->render('AppBundle:Default:offre.html.twig');
+    }
+
+    /**
+     * @Route("/coordonnee", name="coordonnee")
+     */
+    public function coordonneeAction(Request $request)
+    {
+        if ($request->getMethod() == 'POST') {
+            $data = $request->request->get('request');
+            $em = $this->getDoctrine()->getManager();
+
+            $Information = new Information();
+
+            $User = $this->getUser();
+            if ($User->getId() != null) {
+                $User = $em->getRepository('UserBundle:User')->find($User->getId());
+                $Information->setUser($User);
+            }
+
+            $Information->setAddress($data['adresse']);
+            $Information->setCountry($data['country']);
+            $Information->setFirstname($data['firstname']);
+            $Information->setLastname($data['lastname']);
+            $Information->setPhonefix($data['phonefix']);
+            $Information->setPhonemobile($data['phonemobile']);
+            $Information->setZip($data['zip']);
+        }
+        return $this->render('AppBundle:Default:coordonnee.html.twig');
+    }
+
+    /**
+     * @Route("/paiement", name="paiement")
+     */
+    public function paiementAction()
+    {
+        return $this->render('AppBundle:Default:paiement.html.twig');
     }
 }
